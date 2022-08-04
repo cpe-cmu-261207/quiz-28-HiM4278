6b9128191d72a560a583c097b62512f7087bdf68
pasakorn <pasakorn141@gmail.com>
2022-08-04 14:11:47 +0700

    name

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const btnRandom = document.querySelector("#btn-random");
+const names = document.querySelector("#name");
+const Enail = document.querySelector("#p-email");
+const profile = document.querySelector("#img-profile");
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  names.innerText =
+    resp.data.results[0].name.first + resp.data.results[0].name.last;
+  profile.src = resp.data.results[0].picture.large;
+  Enail.innerText = resp.data.results[0].email;
 }
+
+btnRandom.onclick = async () => {
+  callApi();
+};
